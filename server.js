3e4c37ffb2de350039dfaca88850999daead2d9a
Alex Yeh <alexlyeh@gmail.com>
2016-02-06 13:36:51 -0500

    Basic inputs and commands

--- server.js
+++ server.js
@@ -4,43 +4,6 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-  // // messages, username, chat id are Strings, otherUsernames is array of Strings
-  // function read(message, username, thread_id, userId, otherUsernames, otherIds, callback) {
-  //   // Default chat object or existing one
-  //   // And set the global object
-  //   currentChat = chats[thread_id] = chats[thread_id] || {
-  //     lists: {},
-  //     scores: {},
-  //     existingChat: false
-  //   };
-  //   if(!currentChat.lists) currentChat.lists = {};
-  //   if(!currentChat.scores) currentChat.scores = {};
-  //   if(!currentChat.reminders) currentChat.reminders = [];
-  //   if(!users[userId]) users[userId] = {};
-
-    
-  //   currentThreadId = thread_id;
-  //   currentUserId = userId;
-  //   currentUsername = username;
-  //   currentOtherUsernames = otherUsernames;
-
-  //   // Remove one Marc
-  //   if(currentOtherUsernames.indexOf("Marc") !== -1) {
-  //     currentOtherUsernames.splice(currentOtherUsernames.indexOf("Marc"), 1);
-  //   }
-
-  //   // Remove marc from this list
-  //   currentOtherIds = otherIds.filter(function(v) {return v !== MARC_ID;});
-
-  //   var availableCommands = allCommands.default;
-  //   // if we have data for the user, and he has talked in this current chat
-  //   // and his state isn't null, then we use the appropriate set of commands
-  //   // for the given state
-  //   if(users[currentUserId] && users[currentUserId][thread_id] && users[currentUserId][thread_id].state) {
-  //     availableCommands = allCommands[users[currentUserId][thread_id].state];
-  //   }
-
-
 login({email: "alexyuiop@gmail.com", password: "tartanhacks"}, function callback (err, api) {
     if(err) return console.error(err);
     console.log(":O");
@@ -48,14 +11,46 @@ login({email: "alexyuiop@gmail.com", password: "tartanhacks"}, function callback
     api.getUserID("Alex Yeh", function(err, data) {
     	if(err) return callback(err);
 
-    	// if (!currentChat.existingChat){
-     //  		currentChat.existingChat = true;
-     //  		api.sendMessage("Hey I'm a chatbot and here to help. Type '/help' for some useful commands!", thread_id);
-    	// }
-    	
         // Send the message to the best match (best by Facebook's criteria)
         var threadID = data[0].userID;
-        api.sendMessage("eeeeeeeeee", threadID);
+        api.sendMessage("hi", threadID);
+    });
+
+    api.setOptions({listenEvents: true});
+ 	var coins = 0
+    var stopListening = api.listen(function(err, event) {
+        if(err) return console.error(err);
+ 		console.log(event);
+        switch(event.type) {
+          case "message":
+          	var call = event.body.substring(0, 6);
+          	var text_string = event.body.replace("Alfred ", "");
+          	if (call == "Alfred") {
+	            if(text_string === 'shut up' || text_string == 'stop') {
+	              api.sendMessage("Ok see you later", event.threadID);
+	              return stopListening();
+	            }
+	        } 
+	        else {
+	            if(event.body == '/add coin') {
+	            	coins++;
+	              	api.sendMessage("I now have " + coins + "! I am rich.", event.threadID);
+	            } else if(event.body === '/takeaway coin') {
+	            	coins--;
+	              	api.sendMessage("I now have " + coins + "! I am poor.", event.threadID);
+	            } else {
+	            	api.markAsRead(event.threadID, function(err) {
+	              		if(err) console.log(err);
+	            	});
+	            	api.sendMessage(event.body, event.threadID);
+	            }
+	        }
+	        break;
+          case "event":
+          	api.sendMessage("Hey I'm a chatbot and here to help. Type '/help' for some useful commands!", thread_id);
+            console.log(event);
+            break;
+        }
     });
 });
 
